711f3aff541524aa4a1dd7c3a278a1ac7415455a
Lucas S <sushiguy132@gmail.com>
2016-10-20 09:06:53 -0700

    added if statements

--- encoder.js
+++ encoder.js
@@ -3,6 +3,28 @@ var letters = {
 };
 
   $("#inputArea").keydown(function(e) {
-      var inputText = document.getElementById("inputText").value;
-   document.getElementById("textArea").innerHTML = inputText;
+    if ($("input:checked").val() == "echo")  {
+      $("#textArea").append(translation[e.keycode]);
+    }
+
+if ($("input:checked").val() == "shiftcipher")  {
+
+}
+
+
+if ($("input:checked").val() == "heiroglyphics")  {
+$("#textArea").append("<img src = 'images/heiroglyphics" + something + ".gif'>")
+
+}
+
+if ($("input:checked").val() == "runes") {
+
+}
+
+
+});
+$("#translationInputText").keydown(function(e) {
+  if (e.KeyCode == 13) {
+
+  }
 });
